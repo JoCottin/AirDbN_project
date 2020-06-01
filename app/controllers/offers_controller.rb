b53e1fb74c12645d1f528dc92d85e977ea0cef18
Clement Guibaud <clement.guibaud@kedgebs.com>
2020-06-01 19:01:33 +0200

    offers controler plus seed offer, plus index.html.erb view

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,3 +1,30 @@
 class OffersController < ApplicationController
+  def index
+    @offers = Offer.all
+  end
 
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :price_per_day)
+  end
 end
