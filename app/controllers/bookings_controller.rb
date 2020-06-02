2957f27332b62934c339942a39495c191e32b157
Arthur Imbert <arthur.imbert@kedgebs.com>
2020-06-02 17:16:30 +0200

    terminado

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,28 @@
 class BookingsController < ApplicationController
-  def index
-    @bookings = Booking.all
-  end
 
   def show
     @booking = Booking.find(params[:id])
   end
 
   def new
+    @offer = Offer.find(params[:offer_id])
     @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
-    @offer = Offer.find(current_offer.id)
-    @booking.offer = @offer
+    @booking.user = current_user
+    @booking.offer = Offer.find(params[:offer_id])
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to dashboard_path
     else
       render :new
     end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to offers_path
   end
 
   private
