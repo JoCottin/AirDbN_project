37ed4f45dfa795fc9b450254f70bffeca103de32
Joseph Cottin <joseph.cottin@kedgebs.com>
2020-06-02 11:57:57 +0200

    schema clean

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_06_01_175512) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "offers", force: :cascade do |t|
-    t.string "title"
-    t.integer "price_per_day"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_offers_on_user_id"
-  end
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -33,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_06_01_175512) do
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
+
   create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
@@ -43,6 +35,17 @@ ActiveRecord::Schema.define(version: 2020_06_01_175512) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
+
+  create_table "offers", force: :cascade do |t|
+    t.string "title"
+    t.integer "price_per_day"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -56,7 +59,7 @@ ActiveRecord::Schema.define(version: 2020_06_01_175512) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  add_foreign_key "offers", "users"
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "offers", "users"
 end
