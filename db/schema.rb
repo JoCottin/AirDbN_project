7e5f38b51edea6cb98bf25453d6ec5fda47eda52
Joseph Cottin <joseph.cottin@kedgebs.com>
2020-06-03 10:37:11 +0200

    photo added to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_082942) do
+ActiveRecord::Schema.define(version: 2020_06_03_081746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_06_02_082942) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.string "photo"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
